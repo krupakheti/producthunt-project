dcc019c2dad388453d3e4f484b5c98469f9e67e6
krupakheti <kundan.rupakheti99@gmail.com>
2019-03-31 00:01:16 -0500

    Polish

--- producthunt/urls.py
+++ producthunt/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name="home" ),
     path('accounts/', include('accounts.urls')),
+    path('products/', include('products.urls')),
 ]
